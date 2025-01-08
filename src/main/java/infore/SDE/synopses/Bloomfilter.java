d22e3f490dde90bf5cd12288488ec70d6d09d006
itasio <61111336+itasio@users.noreply.github.com>
2025-01-08 19:06:11 +0200

    feat: estimate multiple Bloomfilter Synopses.

--- src/main/java/infore/SDE/synopses/Bloomfilter.java
+++ src/main/java/infore/SDE/synopses/Bloomfilter.java
@@ -7,51 +7,82 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import infore.SDE.messages.Estimation;
 import infore.SDE.messages.Request;
 
+
 import java.io.IOException;
 
 public class Bloomfilter extends Synopsis{
- private BloomFilter bm;
- 
- public Bloomfilter(int uid, String[] parameters) {
-	 super(uid,parameters[0],parameters[1],parameters[2]);
-	 bm = new BloomFilter( Integer.parseInt(parameters[3]), Double.parseDouble(parameters[4]));
-	 
- }
-	@Override
-	public void add(Object k) {
+    private BloomFilter bm;
 
-		//ObjectMapper mapper = new ObjectMapper();
-		JsonNode node = (JsonNode)k;
+    public Bloomfilter(int uid, String[] parameters) {
+        super(uid,parameters[0],parameters[1],parameters[2]);
+        bm = new BloomFilter( Integer.parseInt(parameters[3]), Double.parseDouble(parameters[4]));
+
+    }
+
+    private Bloomfilter(Bloomfilter bmSketch){
+        super(bmSketch.SynopsisID, bmSketch.keyIndex, bmSketch.valueIndex, bmSketch.operationMode);
+        bm = bmSketch.bm;
+    }
+
+    @Override
+    public void add(Object k) {
+
+        //ObjectMapper mapper = new ObjectMapper();
+        JsonNode node = (JsonNode)k;
         /*try {
             node = mapper.readTree(j);
         } catch (IOException e) {
             e.printStackTrace();
         } */
-		String key = node.get(this.keyIndex).asText();
-		bm.add(key);
-		
-	}
-	
-	@Override
-	public String estimate(Object k) {
-		 if(bm.isPresent((Double.toString((double)k))))
-		return "1";
-		return "0";
+        String key = node.get(this.keyIndex).asText();
+        bm.add(key);
 
-	}
+    }
 
-	public Estimation estimate(Request rq) {
+    @Override
+    public String estimate(Object k) {
+        boolean result;
+        if (k instanceof String){
+            result = bm.isPresent((String) k);
+        } else if (k instanceof Number) {
+            result = bm.isPresent(k.toString());
+        }else {
+            try {
+                result = bm.isPresent((Double.toString((double)k)));
+            }catch (ClassCastException e){
+                throw new IllegalArgumentException("Parameter " + k + " couldn't be converted to double ",e);
+            }catch (Exception e){
+                throw new RuntimeException("An unexpected error occurred", e);
+            }
+        }
+        if(result)
+            return "1";
+        return "0";
 
-		return new Estimation(rq, bm.isPresent(rq.getParam()[0]), Integer.toString(rq.getUID()));
-	}
+    }
+
+    public Estimation estimate(Request rq) {
+        if (rq.getRequestID() % 10 == 8){
+            return new Estimation(rq,this,rq.getParam()[2]);	//param[2] is the original datasetKey of the request (needed in ReduceFlatMap)
+        }
+        return new Estimation(rq, bm.isPresent(rq.getParam()[0]), Integer.toString(rq.getUID()));
+    }
 
 
 
-	@Override
-	public Synopsis merge(Synopsis sk) {
-		// TODO Auto-generated method stub
-		return sk;
-	}
+    @Override
+    public Synopsis merge(Synopsis sk) {
+        Bloomfilter mergedSyn = new Bloomfilter(this);
+        try {
+            mergedSyn.bm = (BloomFilter) mergedSyn.bm.merge(((Bloomfilter) sk).bm);
+        } catch (ClassCastException e){
+            throw new IllegalArgumentException("Synopses must be of the same kind to be merged");
+        }
+        if (mergedSyn.bm == null){
+            throw new IllegalArgumentException("Synopsis given for merge cannot be null");
+        }
+        return mergedSyn;
+    }
 
 
 }
